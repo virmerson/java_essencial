fe3f739202d680713b83172586446a419f01b704
virmerson <virmerson@hightechcursos.com.br>
2016-05-11 18:37:49 -0400

    Repetição 1000 vezes

---
+++ src/loops/Main.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//before
+		
+		for (int count=0,  x=10    ; count<4;    count++,x++ ){
+			System.out.println("Hello " + count);
+			
+		}	
+		
+		
+		
+		//after
+			
+	}
+
+}
