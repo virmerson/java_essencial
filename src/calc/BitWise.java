e4f5d84dd6a92173114dd12ba6c2d116d7c6c54f
virmerson <virmerson@hightechcursos.com.br>
2016-05-11 12:55:20 -0400

    Operações com bitWise

--- src/calc/BitWise.java
+++ src/calc/BitWise.java
@@ -7,12 +7,56 @@ public class BitWise {
 		int a =60;
 		int b= 13;
 		
-		int c =  a&b;
+		
 		
 		
 		System.out.println("60 = "+  Integer.toBinaryString(a));
 		System.out.println("13 = "+  Integer.toBinaryString(b));
-		System.out.println("12 = " + Integer.toBinaryString(c));
+		
+		int c =  a&b;
+		String binaryC = Integer.toBinaryString(c);
+		int decimalC = Integer.parseInt(binaryC, 2);
+		
+		System.out.println("a&b = " + binaryC + " Decimal:" +  decimalC);
+		
+		c = a|b ;
+		binaryC = Integer.toBinaryString(c);
+		decimalC = Integer.parseInt(binaryC, 2);
+		
+		System.out.println("a|c= " + binaryC + " Decimal:" + decimalC );
+		
+		c = a^b ;
+		binaryC = Integer.toBinaryString(c);
+		decimalC = Integer.parseInt(binaryC, 2);
+		
+		System.out.println("a|c= " + binaryC + " Decimal:" + decimalC );
+		
+		 c =~a ;
+		binaryC = Integer.toBinaryString(c);
+	
+		System.out.println(binaryC);
+		System.out.println("~a= " + binaryC + " Decimal:" + c );
+		
+		c = a << 2;
+		binaryC = Integer.toBinaryString(c);
+		System.out.println(binaryC);
+		System.out.println("a<<2= " + binaryC + " Decimal:" + c );
+		
+			
+		System.out.println("-------- Deslocamento-----------");
+		a = -60;
+		
+		/*c = a >> 2;  // Manter o sinal
+		binaryC = Integer.toBinaryString(c);
+		System.out.println("a=" + Integer.toBinaryString(a)+  " \na>>2= " + binaryC + " \nDecimal:" + c );
+		*/
+		
+		c = a >>> 2; //Sem sinal
+		binaryC = Integer.toBinaryString(c);
+		System.out.println(binaryC);
+		System.out.println("a=" + Integer.toBinaryString(a)+  " \n a>>>2= " + binaryC + " \nDecimal:" + c );
+			
+	
 		
 	}
 
